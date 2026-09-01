8e60ce0306906f32a3dc599d12bb3c55f34fefee
Riccardo Oro <riccardo.oro@gmail.com>
2026-09-01 17:54:49 +0200

    [ADD] some exe

---
+++ Esercizi/Base/CalcolatriceStatica.java
@@ -0,0 +1,12 @@
+package Esercizi.Base;
+
+
+/**
+ * Problema:  Creare una calcolatrice che abbia le funzionalità sotto forma di metodi statici (usare sempre principi OOP [incapsulamento polimorfismo etc])
+ * NOTA: i metodi si intende somma, sottrazione, moltiplicazione etc 
+ * AVANZATA: Se si vuole richiedere gli input all'utente sono punti in più 
+ * CalcolatriceStatica
+ */
+public  class CalcolatriceStatica {
+   
+}
---
+++ Esercizi/Base/EreditaAnimale.java
@@ -0,0 +1,19 @@
+package Esercizi.Base;
+
+
+/**
+ * Sviluppare una classe Animal che ha come attributi nome dentificativo, verso. 
+ * Questa classe deve essere estesa da una classe EreditaAnimale che aggiugne come attributo  specie e identificativo_specie, in fine questa classe va specializzata in una classe Cane e Gatto. 
+ * L'obbiettivo è quello di creare Cani e gatti  a partire dalle classi padre, una classe più grezza che si specializza. 
+ * Nota aggiungere un metodo FaiVerso alla classe EreditaAnimale.
+ * ATTENZIONE!!: Utilizzare i principi cardine della programmazione OOP (incapsulamento, polimorfismo e ereditarità).  
+ * EreditaAnimale
+ */
+class Animal{
+    
+}
+//modifica il file in modo tale che funzioni anche etc questo è un file bozza fatto male solo per farvi avere le consegne  
+
+public class EreditaAnimale {
+    
+}
